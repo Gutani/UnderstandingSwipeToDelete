ad8345b8d7bda4966c059a2d5fef88e711bb5e9f
Gustavo Antunes <gustavoamalheiros@gmail.com>
2022-02-12 20:39:34 -0300

    Initial Commit

---
+++ SwipeToDeleteCOntextMenuAndActionSheets/SwipeToDeleteCOntextMenuAndActionSheetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeToDeleteCOntextMenuAndActionSheetsApp.swift
+//  SwipeToDeleteCOntextMenuAndActionSheets
+//
+//  Created by Gustavo Malheiros on 12/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SwipeToDeleteCOntextMenuAndActionSheetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
